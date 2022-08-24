0028de6e7b89c6a871808ab760e00e0e8e1fbc09
Mustafa Erhan Portakal <73031908+MustafaEP@users.noreply.github.com>
2022-08-24 10:11:27 +0300

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbxName = new System.Windows.Forms.TextBox();
+            this.tbxAge = new System.Windows.Forms.TextBox();
+            this.tbxSurname = new System.Windows.Forms.TextBox();
+            this.tbxTelNumber = new System.Windows.Forms.TextBox();
+            this.tbxJob = new System.Windows.Forms.TextBox();
+            this.cbxCity = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(113, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(113, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Soyadı";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(114, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Yaş";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(114, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Şehir";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(114, 205);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Tel";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(114, 241);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Meslek";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // tbxName
+            // 
+            this.tbxName.Location = new System.Drawing.Point(231, 56);
+            this.tbxName.Name = "tbxName";
+            this.tbxName.Size = new System.Drawing.Size(161, 22);
+            this.tbxName.TabIndex = 6;
+            // 
+            // tbxAge
+            // 
+            this.tbxAge.Location = new System.Drawing.Point(231, 130);
+            this.tbxAge.Name = "tbxAge";
+            this.tbxAge.Size = new System.Drawing.Size(161, 22);
+            this.tbxAge.TabIndex = 7;
+            // 
+            // tbxSurname
+            // 
+            this.tbxSurname.Location = new System.Drawing.Point(231, 93);
+            this.tbxSurname.Name = "tbxSurname";
+            this.tbxSurname.Size = new System.Drawing.Size(161, 22);
+            this.tbxSurname.TabIndex = 8;
+            // 
+            // tbxTelNumber
+            // 
+            this.tbxTelNumber.Location = new System.Drawing.Point(231, 199);
+            this.tbxTelNumber.Name = "tbxTelNumber";
+            this.tbxTelNumber.Size = new System.Drawing.Size(161, 22);
+            this.tbxTelNumber.TabIndex = 9;
+            // 
+            // tbxJob
+            // 
+            this.tbxJob.Location = new System.Drawing.Point(231, 235);
+            this.tbxJob.Name = "tbxJob";
+            this.tbxJob.Size = new System.Drawing.Size(161, 22);
+            this.tbxJob.TabIndex = 10;
+            // 
+            // cbxCity
+            // 
+            this.cbxCity.FormattingEnabled = true;
+            this.cbxCity.Items.AddRange(new object[] {
+            "İstanbul",
+            "Ankara",
+            "İzmir",
+            "Bursa",
+            "Adana"});
+            this.cbxCity.Location = new System.Drawing.Point(231, 166);
+            this.cbxCity.Name = "cbxCity";
+            this.cbxCity.Size = new System.Drawing.Size(161, 24);
+            this.cbxCity.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(231, 310);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 68);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Kaydet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(231, 56);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(161, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(598, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cbxCity);
+            this.Controls.Add(this.tbxJob);
+            this.Controls.Add(this.tbxTelNumber);
+            this.Controls.Add(this.tbxSurname);
+            this.Controls.Add(this.tbxAge);
+            this.Controls.Add(this.tbxName);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbxName;
+        private System.Windows.Forms.TextBox tbxAge;
+        private System.Windows.Forms.TextBox tbxSurname;
+        private System.Windows.Forms.TextBox tbxTelNumber;
+        private System.Windows.Forms.TextBox tbxJob;
+        private System.Windows.Forms.ComboBox cbxCity;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using word = Microsoft.Office.Interop.Word;
+using System.Reflection;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            object oMissing = System.Reflection.Missing.Value;
+            object documantFinal = "\\endofdoc";
+
+            word.Application creat;
+            word.Document document;
+
+            creat = new word.Application();
+            creat.Visible = true;
+
+            document = creat.Documents.Add(ref oMissing);
+
+            word.Paragraph name;
+            name = document.Content.Paragraphs.Add(ref oMissing);
+            name.Range.Text = "Ad: " + tbxName.Text;
+            name.Format.SpaceAfter = 10;
+            name.Range.InsertParagraphAfter();
+
+            word.Paragraph surname;
+            object orng = document.Bookmarks.get_Item(ref documantFinal).Range;
+            surname = document.Content.Paragraphs.Add(ref orng);
+            surname.Range.Text = "Soyad: " + tbxSurname.Text;
+            surname.Format.SpaceAfter = 10;
+            surname.Range.InsertParagraphAfter();
+
+            word.Paragraph age;
+            orng = document.Content.Bookmarks.get_Item(ref documantFinal).Range;
+            age = document.Content.Paragraphs.Add(ref orng);
+            age.Range.Text = "Yaş: " + tbxAge.Text;
+            age.Format.SpaceAfter = 10;
+            age.Range.InsertParagraphAfter();
+
+            word.Paragraph city;
+            orng = document.Content.Bookmarks.get_Item(ref documantFinal).Range;
+            city = document.Content.Paragraphs.Add(ref orng);
+            city.Range.Text = "Şehir: " + cbxCity.Text;
+            city.Format.SpaceAfter = 10;
+            city.Range.InsertParagraphAfter();
+
+            word.Paragraph tel;
+            orng = document.Content.Bookmarks.get_Item(ref documantFinal).Range;
+            tel = document.Content.Paragraphs.Add(ref orng);
+            tel.Range.Text = "Tel: " + tbxTelNumber.Text;
+            tel.Format.SpaceAfter = 10;
+            tel.Range.InsertParagraphAfter();
+
+            word.Paragraph job;
+            orng = document.Content.Bookmarks.get_Item(ref documantFinal).Range;
+            job = document.Content.Paragraphs.Add(ref orng);
+            job.Range.Text = "Meslek: " + tbxJob.Text;
+            job.Format.SpaceAfter = 10;
+            job.Range.InsertParagraphAfter();
+
+        }
+    }
+}
